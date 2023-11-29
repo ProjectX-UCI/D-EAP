8003271ade740672ef75e58b5713e62fee92e3ac
Alex Wong <alexcw20010923@gmail.com>
2023-11-29 14:08:38 -0800

    feat: add Evaluate class for performance evaluations

--- utils/evaluation_utils.py
+++ utils/evaluation_utils.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import torch
+import time
+from memory_profiler import profile
 
 def plotDF(df,columns,title,ylabel,plot_figure=False,save_figure=False):
     df = pd.DataFrame(df, columns=columns)
@@ -11,3 +14,55 @@ def plotDF(df,columns,title,ylabel,plot_figure=False,save_figure=False):
     
     if save_figure:
         plt.savefig("./results/%s.jpg"%title)
+
+class Evaluator:
+    def __init__(self, model, data_loader, device):
+        self.model = model
+        self.data_loader = data_loader
+        self.device = device
+
+    def evaluate(self, mode = "Validation") -> str:
+        '''
+        mode: "Validation" or "Test"
+        Evaluator.evaluate() returns the accuracy of the model
+        '''
+        self.model.eval()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for inputs, labels in self.data_loader:
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                outputs = self.model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        accuracy = correct / total
+        return f'{mode} Accuracy: {accuracy * 100:.2f}%'
+
+    def latency(self, num_iterations = 100) -> str:
+        '''
+        num_iterations: number of iterations to run inference on
+        Evaluator.latency() returns the average latency of the model
+        '''
+        self.model.eval()
+        start_time = time.time()
+        for i, (inputs, _) in enumerate(self.data_loader):
+            if i >= num_iterations:
+                break
+            inputs = inputs.to(self.device)
+            with torch.no_grad():
+                outputs = self.model(inputs)
+        end_time = time.time()
+        latency = (end_time - start_time) / num_iterations
+        return f'Average Latency: {latency * 1000:.2f} ms'
+
+    @profile
+    def memory_usage(self) -> None:
+        # initialize evaluation Object in main and run
+        # python -m memory_profiler main_experiment.py
+        self.model.eval()
+        for inputs, _ in self.data_loader:
+            inputs = inputs.to(self.device)
+            with torch.no_grad():
+                outputs = self.model(inputs)

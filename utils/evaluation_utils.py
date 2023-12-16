ffd10da89dbae202f6351c6343f7a8a9ea0ed547
Alex Wong <alexcw20010923@gmail.com>
2023-12-15 20:04:19 -0800

    FIX: trained on cpu and inference on gpu error

--- utils/evaluation_utils.py
+++ utils/evaluation_utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import torch
-import time
+import torch, time
+import subprocess as sp
 # from memory_profiler import profile
 
 def plotDF(df,columns,title,ylabel,plot_figure=False,save_figure=False):
@@ -25,12 +25,14 @@ def plotDF(df,columns,title,ylabel,plot_figure=False,save_figure=False):
     if save_figure:
         plt.savefig("./results/%s.jpg"%title)
 
+
 class Evaluator:
     """Class for evaluating regularizers in terms of various measurement benchmarks
     """
 
     def __init__(self, model, model_path, data_loader, device='cpu'):
         self.model = model
+        self.model.to(device)
         self.model.load_state_dict(torch.load(model_path))
         self.model.eval()
 
@@ -74,8 +76,7 @@ class Evaluator:
             end_time = time.time()
             latency = (end_time - start_time) / iterations
             latency *= 1000
-        
-        return accuracy,latency
+        return accuracy, latency
 
     # @profile
     # def memory_usage(self) -> None:
